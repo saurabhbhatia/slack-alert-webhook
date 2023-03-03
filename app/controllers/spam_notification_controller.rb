fdeab1f5cd6c71df25902aea589ebdae3e09d337
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-03-04 00:41:17 +1100

    Convert Slack post to a service object

--- app/controllers/spam_notification_controller.rb
+++ app/controllers/spam_notification_controller.rb
@@ -1,8 +1,7 @@
 class SpamNotificationController < ApplicationController
   def create
     if params.dig("Type").eql?("SpamNotification")
-      client = Slack::Web::Client.new
-      client.chat_postMessage(channel: '#spam-notifications', text: "Spam Email: #{spam_email}", as_user: true)
+      PostMessageToSlackService.call(email: spam_email)
       render json: {message: "success"}, status: :created
     else
       render json: {}, status: :unprocessable_entity
---
+++ app/services/post_message_to_slack_service.rb
@@ -0,0 +1,14 @@
+class PostMessageToSlackService
+  def self.call(email:)
+    new(email).call
+  end
+
+  def initialize(email)
+    @email = email
+  end
+
+  def call
+    client = Slack::Web::Client.new
+    client.chat_postMessage(channel: '#spam-notifications', text: "Spam Email: #{@email}", as_user: true)
+  end
+end
